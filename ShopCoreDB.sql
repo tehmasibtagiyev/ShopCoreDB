0a9f596e84a7fb5ad1aededdc2d86af91ba7ace1
tehmasibtagiyev <tehmasibtagiyev1829@gmail.com>
2025-11-11 00:13:46 +0400

    AddedData

--- ShopCoreDB.sql
+++ ShopCoreDB.sql
@@ -1,8 +1,114 @@
 
+USE ShopCoreDB
 
 CREATE TABLE OrderProducts
 (
 orderID INT FOREIGN KEY REFERENCES Orders(Id),
 productID INT FOREIGN KEY REFERENCES Products(Id)
 )
+
+
+SELECT * FROM Payments
+DROP TABLE Payments 
+DELETE FROM Payments
+TRUNCATE TABLE Payments
+ALTER TABLE Orders DROP CONSTRAINT FK_Orders_Payments
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Payments 
+FOREIGN KEY (paymentID) REFERENCES Payments(Id)
+INSERT INTO Payments VALUES
+('With Credit Card'),
+('With Personal Card'),
+('With ATM')
+
+-- ========================
+
+SELECT * FROM Baskets
+--Added data
+
+-- ========================
+
+SELECT * FROM Categories
+INSERT INTO Categories VALUES
+('Computers', 'Computer is a programmable electronic'),
+('Phones', 'Phone is a telecommunications device'),
+('Clothes', 'Clothes description explains what an item'),
+('Kitchenware', 'Kitchenware refers to the tools')
+
+-- ========================
+SELECT * FROM Customers
+ALTER TABLE Customers ALTER COLUMN [Password] NVARCHAR(20) -- failed
+ALTER TABLE Customers DROP CONSTRAINT FK_Customers_Baskets
+ALTER TABLE Orders DROP CONSTRAINT FK_Orders_Customers
+ALTER TABLE Customers DROP CONSTRAINT Check_Password
+ALTER TABLE Customers ALTER COLUMN [Password] CHAR(8) -- changed
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Customers
+FOREIGN KEY (customerID) REFERENCES Customers(Id)
+ALTER TABLE Customers ADD CONSTRAINT FK_Customers_Baskets
+FOREIGN KEY (basketID) REFERENCES Baskets(ID)
+ALTER TABLE Orders DROP COLUMN Amount
+UPDATE Customers SET Customers.[Password] = NULL
+TRUNCATE TABLE Customers
+
+INSERT INTO Customers ([Name], Email, [Password], basketID) VALUES
+('Tehmasib', 'tahmasibmt8@gmail.com','tagiyev', 8), 
+('Nofel', 'nofelbagi20@gmail.com', 'bagirov2', 5),
+('Emil', 'e.abbasov3645@gmail.com', 'abbsv36', 3),
+('Emil', 'zeynalzade@gmail.com', 'emil518', 1),
+('Xeyal', 'tagiyevxeyal@gmai.com', 'tagizade', 10),
+('Taleh', 'qtaleh30@gmail.com', 'taleh45', 7),
+('Elvin', 'elvn@gmail.com', 'elvin456', 2),
+('Cavidan', 'cvdnhasnli5@gmail.com', 'hesenli5', 4),
+('Aynure', 'tagiyeva@gmail.com', 'tagiyeva', 9),
+('Tehmine', 'thmn4tagi8@gmail.com', 'thmine9a', 6)
+
+-- ========================
+SELECT * FROM Products
+INSERT INTO Products VALUES
+('ASUS ZENBook', 3289, 'A line of premium, thin, and light laptops', 1),
+('Trousers', 56, 'Could refer to two very different things', 3),
+('Refrigerator', 8129, 'A refrigerator is an appliance that cools food', 4),
+('Iphone 16 Pro', 2960, 'Features a larger 6.3-inch Super Retina XDR display', 2),
+('Samsung S24 FE', 4420, 'Snap it, circle it, tap it – discover why it', 2),
+('T-Shirt', 190.5, 'Lightweight, typically cotton-knit pullover', 3),
+('Washing machine', 7930, 'Appliance that automatically cleans clothes using water',4),
+('Acer I7', 2366, 'Taiwanese multinational company that manufactures', 1)
+
+-- ========================
+SELECT * FROM Orders
+ALTER TABLE Orders DROP CONSTRAINT FK_Orders_Customers
+ALTER TABLE Orders DROP CONSTRAINT FK_Orders_Payments
+ALTER TABLE OrderProducts DROP CONSTRAINT FK__OrderProd__order__76969D2E
+TRUNCATE TABLE Orders
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Customers
+FOREIGN KEY (customerID) REFERENCES Customers(Id)
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Payments
+FOREIGN KEY (paymentID) REFERENCES Payments(Id)
+ALTER TABLE OrderProducts ADD CONSTRAINT FK__OrderProd__order__76969D2E
+FOREIGN KEY (orderID) REFERENCES Orders(Id)
+
+INSERT INTO Orders VALUES
+('2025-11-10 21:39:28', 9, 3),
+('2025-11-10 15:21:47', 1, 1),
+('2025-11-07 10:20:56', 5, NULL),
+('2025-11-19 11:18:36', 2, 3),
+('2025-11-24 09:07:01', 8, 1),
+('2025-11-03 12:15:05', 3, NULL),
+('2025-11-16 17:46:22', 10, 2),
+('2025-11-29 21:39:00', 4, 2),
+('2025-10-08 14:32:40', 6, 1),
+('2025-10-18 16:55:00', 7, Null) 
+
+-- ===========================
+
+ALTER TABLE OrderProducts DROP CONSTRAINT FK__OrderProd__order__76969D2E 
+ALTER TABLE OrderProducts DROP CONSTRAINT FK__OrderProd__produ__778AC167
+TRUNCATE TABLE OrderProducts
+ALTER TABLE OrderProducts ADD CONSTRAINT FK__OrderProd__order__76969D2E
+FOREIGN KEY (orderID) REFERENCES Orders(Id)
+ALTER TABLE OrderProducts ADD CONSTRAINT FK__OrderProd__produ__778AC167
+FOREIGN KEY (productID) REFERENCES Products(Id)
+
+
+
+
 
